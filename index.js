9aec66d8e7184e6dab5d1da20ad698f88db4eac7
Thomaz Simão <thomazsimao5@gmail.com>
2023-01-16 16:17:20 -0300

    trabalhando com os cálculos e a lógica

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+
+function formatMoney(value){
+    value = value.toFixed(2) // método para formatação para 2 casas decimais
+    return  '$ ' + value
+}
+
+
 function upDate() {
 
     let bill = Number(document.getElementById("yourBill").value)
@@ -7,6 +14,18 @@ function upDate() {
     let split = document.getElementById('splitInput').value
 
    // console.log({bill,tipPercent,split})
+
+    let tipValue = bill * (tipPercent / 100)
+
+    let  billTotal = bill + tipValue
+
+    let billEach =  billTotal / split
+
+    document.getElementById('tipPercent').innerHTML = tipPercent + ' %'
+    document.getElementById('tipValue').innerHTML =  formatMoney(tipValue)
+    document.getElementById('totalWithTip').innerHTML = formatMoney(billTotal)
+    document.getElementById('splitValue').innerHTML = split
+    document.getElementById('billEach').innerHTML = formatMoney(billEach) 
 
 
 
